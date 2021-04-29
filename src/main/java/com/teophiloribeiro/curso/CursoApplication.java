db1af065002ac311147e4b3a6e491226fbf2695b
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-04-29 20:12:54 -0300

    Criando operacao de instanciacao

--- src/main/java/com/teophiloribeiro/curso/CursoApplication.java
+++ src/main/java/com/teophiloribeiro/curso/CursoApplication.java
@@ -1,13 +1,38 @@
 package com.teophiloribeiro.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.teophiloribeiro.curso.domain.Categoria;
+import com.teophiloribeiro.curso.repositories.CategoriaRepository;
+
+//INSTANCIAR O BD ASSIM QUE A APLICAÇÃO INICIAR
+
 @SpringBootApplication
-public class CursoApplication {
+public class CursoApplication implements CommandLineRunner{
+	
+	@Autowired // instanciado automaticamente
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
+	
+	//Metodo para instanciar o BD
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+	}
+	
 
 }
