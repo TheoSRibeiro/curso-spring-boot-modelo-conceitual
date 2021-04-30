506c974f3807f65c07f0598805c1904926f39c71
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-04-30 14:03:46 -0300

    Estado e Cidade

--- src/main/java/com/teophiloribeiro/curso/CursoApplication.java
+++ src/main/java/com/teophiloribeiro/curso/CursoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.teophiloribeiro.curso.domain.Categoria;
+import com.teophiloribeiro.curso.domain.Cidade;
+import com.teophiloribeiro.curso.domain.Estado;
 import com.teophiloribeiro.curso.domain.Produto;
 import com.teophiloribeiro.curso.repositories.CategoriaRepository;
+import com.teophiloribeiro.curso.repositories.CidadeRepository;
+import com.teophiloribeiro.curso.repositories.EstadoRepository;
 import com.teophiloribeiro.curso.repositories.ProdutoRepository;
 
 //INSTANCIAR O BD ASSIM QUE A APLICAÇÃO INICIAR
@@ -17,12 +21,22 @@ import com.teophiloribeiro.curso.repositories.ProdutoRepository;
 @SpringBootApplication
 public class CursoApplication implements CommandLineRunner{
 	
+	//-----REPOSITORIES------
+	
 	@Autowired // instanciado automaticamente
 	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
-
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	//-----REPOSITORIES------
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
@@ -31,6 +45,7 @@ public class CursoApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		//CATEGORIA E PRODUTO
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
@@ -45,11 +60,29 @@ public class CursoApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		//ESTADO E CIDADE
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
 		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		
+		//Repository categoria
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		
 		//Criar repositorio dos produtos (produtoRepository) e salvar os produtos
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		//Repository das cidades e estados
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 		
 		
 	}
